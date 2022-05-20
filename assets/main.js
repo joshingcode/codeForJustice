7ed8bdbe065115ef64afc2fdacadfa7af9b7b2ab
josh <102491975+joshingcode@users.noreply.github.com>
2022-05-20 08:36:00 -0400

    update

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,6 @@
 document.querySelector('.submitButton').addEventListener('click',finish())
 
 function finish(){
-    document.querySelector('')
+    
+    document.querySelector('.thankYou').innerHTML="Thank You for contacting us someone from Cardinal Group reach out to you within 24 busniess hours."
 }
